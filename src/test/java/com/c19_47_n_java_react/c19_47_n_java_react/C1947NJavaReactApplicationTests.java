f253f81adbeb84ed407516ce4e2296211fc77b13
Pepe Lambrechts <100524075+alt165@users.noreply.github.com>
2024-07-10 15:29:17 -0300

    Add files via upload

---
+++ src/test/java/com/c19_47_n_java_react/c19_47_n_java_react/C1947NJavaReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c19_47_n_java_react.c19_47_n_java_react;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C1947NJavaReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
